5bb55dcec4ba2a5a777533ae375ea145f6012301
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2022-08-16 11:43:15 +0100

    changes

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
     //Localhost
-        // defined('DB_SERVER')	? null:define("DB_SERVER", "localhost");
-        // defined('DB_USER')	? null:define("DB_USER", "root");
-        // defined('DB_PASS')	? null:define("DB_PASS", "");
-        // defined('DB_NAME')	? null:define("DB_NAME", "omotropn_yenreach");
-        // defined('SITE_NAME')	? null:define("SITE_NAME", "");
-        // defined('FOOTER_CONTENT')	? null:define("FOOTER_CONTENT", "");
+        defined('DB_SERVER')	? null:define("DB_SERVER", "localhost");
+        defined('DB_USER')	? null:define("DB_USER", "root");
+        defined('DB_PASS')	? null:define("DB_PASS", "");
+        defined('DB_NAME')	? null:define("DB_NAME", "omotropn_yenreach");
+        defined('SITE_NAME')	? null:define("SITE_NAME", "");
+        defined('FOOTER_CONTENT')	? null:define("FOOTER_CONTENT", "");
 
     //Live
-    defined('DB_SERVER')	? null:define("DB_SERVER", "localhost");
-    defined('DB_USER')	? null:define("DB_USER", "omotropn_yenreach");
-    defined('DB_PASS')	? null:define("DB_PASS", "ThinTree21***");
-    defined('DB_NAME')	? null:define("DB_NAME", "omotropn_yenreach");
-    defined('SITE_NAME')	? null:define("SITE_NAME", "");
-    defined('FOOTER_CONTENT')	? null:define("FOOTER_CONTENT", "");
+    // defined('DB_SERVER')	? null:define("DB_SERVER", "localhost");
+    // defined('DB_USER')	? null:define("DB_USER", "omotropn_yenreach");
+    // defined('DB_PASS')	? null:define("DB_PASS", "ThinTree21***");
+    // defined('DB_NAME')	? null:define("DB_NAME", "omotropn_yenreach");
+    // defined('SITE_NAME')	? null:define("SITE_NAME", "");
+    // defined('FOOTER_CONTENT')	? null:define("FOOTER_CONTENT", "");
 ?>
